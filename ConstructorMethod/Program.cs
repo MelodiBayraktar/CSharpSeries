92f2c6a91cb0bc8ef92357b24fcf16552aa22074
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2023-11-04 23:37:08 +0300

    “ Constructor ile çalışmalar yapıldı”

--- ConstructorMethod/Program.cs
+++ ConstructorMethod/Program.cs
@@ -4,7 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Customer customer1 = new Customer ()
+        {
+            Id = 1,
+            FirstName="Engin",
+            LastName= "Demiroğ",
+            City = "Ankara"
+        };
+
+        Customer customer3 = new Customer();
+        customer3.Id = 3;
+
+        Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+        Console.WriteLine(customer2.FirstName);
+
+
+        Console.ReadLine();
     }
+  
+}
+class Customer
+{
+    public Customer()
+    {
+
+    }
+    //default constructor
+    public Customer(int id, string firstname, string lastname, string city)
+    {
+        Id = id;
+        FirstName = firstname;
+        LastName = lastname;
+        City = city;
+
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
 }
 
