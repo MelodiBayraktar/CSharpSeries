d68a1b13dbced1961b993ce06a0f1502c4042422
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2023-12-09 21:26:55 +0300

    generic dosyası oluşturuldu

---
+++ CSharpSeries/Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> sehirler = new List<string>();
+        sehirler.Add("Ankara");
+        Console.WriteLine(sehirler.Count);
+        
+        MyList<string> sehirler2 = new MyList<string>();
+        sehirler2.Add("İstanbul");
+        Console.WriteLine(sehirler2.Count);
+    }
+}
+
+class MyList<T>//generic class
+{
+     T[] _array;
+
+     T[] _tempArray;
+     public MyList()
+     {
+         _array = new T[0];
+     }
+
+     public void Add(T item)
+     {
+         _tempArray = _array;
+         _array = new T[_array.Length + 1];
+     }
+
+     public int Count
+     {
+         get { return _array.Length; }
+     }
+}
